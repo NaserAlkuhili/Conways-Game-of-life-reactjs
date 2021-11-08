1cc8d4939ebd78ffe1b9d444532a3c9ca2fe0e26
NaserAlkuhili <k4gam3r@gmail.com>
2021-11-07 22:05:40 -0600

    first commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import produce from "immer";
 
 function App() {
+  const numOfRows = 10;
+  const numOfCol = 10;
+
+
+  // creating grid function
+  const createGrid = (r, c) =>{
+    const rows = [];
+    for(let i = 0; i < r; i++){
+      const column = [];
+      for(let j = 0; j < c; j++){
+        //false means the cell is dead.
+        column.push(false)
+      }
+      rows.push(column);
+    }
+    return rows;
+  }
+  //The generation's state
+  const [gen, SetGen] = useState(createGrid(numOfRows, numOfCol));
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="grid"  style = {{gridTemplateColumns: `repeat(${numOfCol}, ${1250/numOfCol+'px'})`}}>
+        {gen.map((rows, r) => rows.map((column, c) => <div key = {r + "_" + c} className={"cell " + gen[r][c]} style={{height: 650/ {numOfRows}, width:1250/{numOfCol}}} onClick={() => {
+                  const newGen = produce(gen, genCopy => {
+                    genCopy[r][c] = gen[r][c] ? false : true;
+                  });
+                  SetGen(newGen);
+                }}/>))}
+      </div>
+      <div className="btn-container">
+        <div className="btn">Start</div>
+        <div className="btn">Random</div>
+        <div className="btn" onClick={()=>SetGen(createGrid(numOfRows, numOfCol))}>Clear</div>
+      </div>
     </div>
   );
 }
